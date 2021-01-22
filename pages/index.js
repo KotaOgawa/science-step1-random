8ff9e419fca11344b891efde11394d43c73b190c
KotaOgawa <k.ogawa.biz406@gmail.com>
2021-01-22 14:56:09 +0900

    :iphone: レスポンシブ挙動確認

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,14 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div>
+    <div className="sm:text-red-400">
       <Head>
         <title>自由自在理科ランダム</title>
       </Head>
 
       <main>
-        <h1>Hello world!</h1>
+        <h1 className="">Hello world!</h1>
+        <h2>Tailwind効いてないと思ったら違うところ見てた</h2>
       </main>
     </div>
   )
